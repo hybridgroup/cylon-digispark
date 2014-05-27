a7256e506602c3fa1474e510ef4a027196009af1
Andrew Stewart <andrew@stwrt.ca>
2014-05-27 12:02:09 -0700

    Correct #register, remove #driver, add specs

--- test/specs/cylon-digispark.spec.js
+++ test/specs/cylon-digispark.spec.js
@@ -1,17 +1,39 @@
 'use strict';
 
-var module = source('cylon-digispark');
+var module = source('cylon-digispark'),
+    Digispark = source('digispark');
 
-describe("Cylon-Digispark", function() {
-  it("has a register function", function() {
-    expect(module.register).to.be.a('function');
+var GPIO = require('cylon-gpio');
+
+describe("cylon-digispark", function() {
+  describe("#register", function() {
+    var robot;
+
+    beforeEach(function() {
+      robot = { registerAdaptor: spy() };
+      stub(GPIO, 'register');
+      module.register(robot);
+    });
+
+    afterEach(function() {
+      GPIO.register.restore();
+    });
+
+    it("registers the 'digispark' adaptor with the robot", function() {
+      expect(robot.registerAdaptor).to.be.calledWith(
+        'cylon-digispark',
+        'digispark'
+      );
+    });
+
+    it("registers GPIO with the robot", function() {
+      expect(GPIO.register).to.be.calledWith(robot);
+    });
   });
 
-  it("has a #adaptor function", function() {
-    expect(module.adaptor).to.be.a('function');
-  });
-
-  it("has a #driver function", function() {
-    expect(module.driver).to.be.a('function');
+  describe("#adaptor", function() {
+    it("returns a new instance of the Digispark adaptor", function() {
+      expect(module.adaptor({})).to.be.an.instanceOf(Digispark);
+    });
   });
 });
